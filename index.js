87c38c78a9e6a33f5d427c4a23c3c0bac29cb247
roseholmesminton <rose.yvonne216@icloud.com>
2021-07-06 10:11:52 -0400

    Donut Clicker Works

--- index.js
+++ index.js
@@ -1,2 +1,16 @@
-const container = (document.querySelector('.container').innerText =
-  'mmmmmmmmm doughnuts.... ahhhhhhh!');
+import DonutMaker from "./DonutMaker";
+let donutClicker = document.getElementById("donutClicker");
+let numberOfDonuts = document.getElementById("numDonuts");
+let createdDonutMaker = new DonutMaker(0, 0);
+console.log(createdDonutMaker);
+
+
+donutClicker.addEventListener("click", () => {
+  createdDonutMaker.addDonut();
+  numberOfDonuts.innerHTML = createdDonutMaker.numDonuts;
+  console.log(createdDonutMaker.numDonuts);
+});
+
+
+  
+
